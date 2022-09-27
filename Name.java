814a7e00c9983cfe06cc29109dcd4924a6b0a74c
Karthikeyancoding <111740836+Karthikeyancoding@users.noreply.github.com>
2022-09-27 08:44:47 +0530

    Add files via upload

---
+++ Name.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Name
+{
+public static void main(string[]args)
+{
+Scanner in=new scanner(system.in);
+System.out.println("enter the user name");
+String str1=in.nextLine();
+System.out.println("reenter the user name");
+string str2=in.nextLine();
+if(str1.equals(str2))
+{
+system.out.println("user name is valid");
+}
+else
+{
+system.out.println("user name is not valid");}
+}
+}
