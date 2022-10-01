e40c30c9e384b8b1d32c6e8173900db0e64e3ff6
Karthikeyancoding <111740836+Karthikeyancoding@users.noreply.github.com>
2022-09-30 20:25:40 -0700

    Add files via upload

---
+++ Squarecube.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Squarecube
+ {
+    public static void main(String args[]) 
+{
+        Scanner sc = new Scanner(System.in);
+        double num;
+        System.out.print("Enter the number: ");
+        num = sc.nextDouble();
+        System.out.println("Square of " + num + " is: " + Math.pow(num, 2));
+        System.out.println("Cube of " + num + " is: " + Math.pow(num, 3));
+    }
+}
